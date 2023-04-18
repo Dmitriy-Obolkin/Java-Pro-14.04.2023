283994e79d58f09d1c8f2a186aa15af9a6a008cb
Dmytro Obolkin <obolkindm@gmail.com>
2023-04-19 00:13:01 +0300

    Added the Main class to the main package.

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package src.main;
+
+import src.main.homeworks.hw2.Employee;
+
+public class Main {
+    public static void main(String[] args){
+
+        demoHW2();
+    }
+
+    private static void demoHW2(){
+        Employee tom = new Employee("Tom Franklin", "director", "tom-franklin@gmail.com", "4627-3756", 29);
+        System.out.println(tom);
+    }
+}
