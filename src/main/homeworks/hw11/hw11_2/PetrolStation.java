80001119edc5ea004dcbb75cd68df5af17b46417
Dmytro Obolkin <obolkindm@gmail.com>
2023-05-19 16:34:15 +0300

    Deleted redundant call to format() in src.main.homeworks.hw11.hw11_2.PetrolStation.java

--- src/main/homeworks/hw11/hw11_2/PetrolStation.java
+++ src/main/homeworks/hw11/hw11_2/PetrolStation.java
@@ -5,8 +5,8 @@ import java.util.concurrent.Semaphore;
 
 public class PetrolStation {
     private double amount;
-    private Semaphore semaphore;
-    private Random random;
+    private final Semaphore semaphore;
+    private final Random random;
 
     public PetrolStation(double amount){
         this.amount = amount;
@@ -18,17 +18,17 @@ public class PetrolStation {
         semaphore.acquire(); // получить 1 из 3 разрешение
         try{
             if(fuel > amount){
-                System.out.println(String.format("(%s) Not enough fuel. Requested: %.2f, " +
-                        "available: %.2f.", Thread.currentThread().getName(), fuel, amount));
+                System.out.printf("(%s) Not enough fuel. Requested: %.2f, " +
+                        "available: %.2f.%n", Thread.currentThread().getName(), fuel, amount);
                 return;
             }
 
-            System.out.println(String.format("(%s) Refueling started for: %.2f.",
-                    Thread.currentThread().getName(), fuel));
+            System.out.printf("(%s) Refueling started for: %.2f.%n",
+                    Thread.currentThread().getName(), fuel);
             amount -= fuel;
             Thread.sleep((random.nextInt(8) + 3) * 1000);
-            System.out.println(String.format("(%s) Refueling finished. Remaining fuel: %.2f.",
-                    Thread.currentThread().getName(), amount));
+            System.out.printf("(%s) Refueling finished. Remaining fuel: %.2f.%n",
+                    Thread.currentThread().getName(), amount);
 
         } finally {
             semaphore.release(); //освободить 1 разрешение
