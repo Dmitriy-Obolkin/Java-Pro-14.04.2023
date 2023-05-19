17c4713f603188eeb1882ea062ea9d669a4747c3
Dmytro Obolkin <obolkindm@gmail.com>
2023-05-19 12:04:09 +0300

    Added src.main.homeworks.hw11.hw11_1 package.

---
+++ src/main/homeworks/hw11/hw11_1/ThreadSafeList.java
@@ -0,0 +1,31 @@
+package src.main.homeworks.hw11.hw11_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreadSafeList<T> {
+    private List<T> list;
+
+    public ThreadSafeList(){
+        this.list = new ArrayList<>();
+    }
+
+    public synchronized void add(T value){
+        list.add(value);
+    }
+
+    public synchronized  boolean remove(T value) throws IllegalArgumentException{
+        if(!list.remove(value)){
+            throw new IllegalArgumentException("Failed to remove the item. Item not found.");
+        }
+        return true;
+    }
+
+    public synchronized T get(int index) throws IndexOutOfBoundsException{
+        if(index < 0 || index >= list.size()){
+            throw new IndexOutOfBoundsException("Index out of bounds for list.");
+        }
+        return list.get(index);
+    }
+
+}
