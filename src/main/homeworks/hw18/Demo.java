b98d1501a8c1c3959c13cd169b60332cfbb4d0e1
Dmytro Obolkin <obolkindm@gmail.com>
2023-06-19 12:49:00 +0300

    Add tasks 2.1, 2.2 on homework18

--- src/main/homeworks/hw18/Demo.java
+++ src/main/homeworks/hw18/Demo.java
@@ -7,39 +7,50 @@ public class Demo {
     public static void main(String[] args) {
         List<Product> products = new ArrayList<>();
 
-        products.add(new Product("Apple", 100));
-        products.add(new Product("Banana", 50));
-        products.add(new Product("Book", 80));
-        products.add(new Product("Tea", 200));
-        products.add(new Product("Book", 400));
-        products.add(new Product("Apple", 105));
-        products.add(new Product("Milk", 120));
-        products.add(new Product("Cheese", 300));
-        products.add(new Product("Tea", 250));
-        products.add(new Product("Book", 340));
-        products.add(new Product("Banana", 70));
-        products.add(new Product("Book", 90));
-        products.add(new Product("Cheese", 30));
-        products.add(new Product("Tea", 130));
-        products.add(new Product("Coffee", 250));
-        products.add(new Product("Apple", 95));
-        products.add(new Product("Banana", 55));
-        products.add(new Product("Apple", 20));
-        products.add(new Product("Book", 140));
-        products.add(new Product("Book", 350));
+        products.add(new Product("Apple", 100, true));
+        products.add(new Product("Banana", 50, true));
+        products.add(new Product("Book", 80, false));
+        products.add(new Product("Tea", 200, false));
+        products.add(new Product("Book", 400, false));
+        products.add(new Product("Apple", 105, false));
+        products.add(new Product("Milk", 120, false));
+        products.add(new Product("Cheese", 300, true));
+        products.add(new Product("Tea", 250, true));
+        products.add(new Product("Book", 340, true));
+        products.add(new Product("Banana", 70, false));
+        products.add(new Product("Book", 90, true));
+        products.add(new Product("Cheese", 30, false));
+        products.add(new Product("Tea", 130, false));
+        products.add(new Product("Coffee", 250, true));
+        products.add(new Product("Apple", 95, false));
+        products.add(new Product("Banana", 55, true));
+        products.add(new Product("Apple", 20, false));
+        products.add(new Product("Book", 140, true));
+        products.add(new Product("Book", 350, true));
 
         System.out.println("All products: ");
         for (Product product : products) {
             System.out.println(product);
         }
 
+        //1.2
         System.out.println("\nBooks with a price over 250: ");
         List<Product> books = products.stream()
-                .filter(p -> p.getType().toLowerCase().equals("book"))
-                .filter(p -> p.getPrice() > 250)
+                .filter(product -> product.getType().equalsIgnoreCase("book"))
+                .filter(book -> book.getPrice() > 250)
                 .peek(System.out::println)
                 .toList();
 
+        //2.2
+        System.out.println("\nBooks with the possibility of applying a discount and the new price of -10%: ");
+        List<Product> books2 = products.stream()
+                .filter(product -> product.getType().equalsIgnoreCase("book"))
+                .filter(book -> book.isDiscountAvailable())
+                .peek((book) -> book.applyDiscount(10))
+                .peek(System.out::println)
+                .toList();
+
+        System.out.println("\n");
 
     }
 }
--- src/main/homeworks/hw18/Product.java
+++ src/main/homeworks/hw18/Product.java
@@ -1,20 +1,44 @@
 package src.main.homeworks.hw18;
 
 public class Product {
-    private String type;
-    private int price;
+    private final String type;
+    private double price;
+    private final boolean discountAvailable;
 
-    public Product(String type, int price){
+    public Product(String type, double price, boolean discountAvailable){
         this.type = type;
         this.price = price;
+        this.discountAvailable = discountAvailable;
     }
 
     public String getType() {
         return type;
     }
 
-    public int getPrice() {
+    public double getPrice() {
         return price;
+    }
+
+    public boolean isDiscountAvailable() {
+        return discountAvailable;
+    }
+
+    private void setPrice(double newPrice){
+        if(newPrice<0){
+            throw new IllegalArgumentException("The price must be greater than 0");
+        }
+        price = newPrice;
+    }
+
+    public void applyDiscount(double discountPercentage) {
+        if(discountPercentage >= 100){
+            throw new IllegalArgumentException("The discount should be less than 100%");
+        }
+
+        if (isDiscountAvailable()) {
+            double discountAmount = price * discountPercentage / 100;
+            setPrice(price - discountAmount);
+        }
     }
 
 
